3f56b6d6ab3d9c8f874ad36000b65efd28808be9
Younes bayoud <younesbayoud@Youness-Air.hsd1.il.comcast.net>
2024-06-06 23:57:38 -0500

    adding new features to the calculator on the js file

--- Calcul.js
+++ Calcul.js
@@ -1,14 +1,57 @@
 let display = document.getElementById('display');
 
-let firstOperand = '';
-let secondOperand = '';
+let firstNumber = '';
+let secondNumber = '';
 let currentNumber = 0;
+let operator = '';
 
-function AppendNumber(number) {
-    return currentNumber += number;
-    display.innerText = currentNumber;
-    firstOperand ='';
-    secondOperand = '';
-    currentNumber =0;
+const  AppendNumber = (number) =>  {
+    currentNumber += number;
+     display.innerText = currentNumber;
+   
+}
+
+const Operation = (op) => {
+    if ( currentNumber ==='' && op !== '-' ) return;
+    if ( currentNumber !== '' && op === '')
+    return firstNumber = currentNumber;
+else if ( currentNumber !== '' && op) return CalculatorResult()
+firstNumber ='';
+secondNumber ='';
+currentNumber = 0;
+} 
+
+const  CalculatorResult = () => {
+  if (currentNumber === '' || firstNumber === '') return;
+  secondNumber = currentNumber;
+
+  let result = 0;
+
+  switch (operator) {
+
+    case '+' : result = parseFloat(firstNumber) + parseFloat(secondNumber); break;
+    case '-' : result = parseFloat(firstNumber) - parseFloat(secondNumber); break;
+    case '*' : result = parseFloat(firstNumber) * parseFloat(secondNumber); break;
+    case '/' : result = parseFloat(firstNumber) / parseFloat(secondNumber); break;
+
+
+  }
+
+  display.innerText = result;
+  firstNumber ='';
+  secondNumber ='';
+  operator = '';
+  currentNumber = '';
+
+
+}
+
+const ClearButton = () => {
+    firstNumber ='';
+    secondNumber ='';
+    operator = '';
+    currentNumber = '';
+    display.innerText = '0';
+
 }
 
